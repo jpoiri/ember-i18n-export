d2d1e3d5e3eb777c7f49175de724dcf85ede13ae
jpoiri <justin.poirier@gmail.com>
2017-06-13 15:36:19 -0300

    Update package.json

--- ember-i18n-export.js
+++ ember-i18n-export.js
@@ -1,154 +1,264 @@
-var program = require('commander');
-var fs = require('fs');
-var flatten = require('flat');
-var csvWriter = require('csv-write-stream');
-var wordCount = require('wordcount');
-var arrayContains = require('array-contains');
+#!/usr/bin/env node
+'use strict';
 
-var ENCODING = 'UTF-8';
-var TRANSLATION_BASE_DIR = 'app/locales/';
-var TRANSLATION_FILE_NAME = 'translations.js';
-var OUTPUT_DIR  = 'i18n-exports';
-var OUTPUT_FILE = 'i18n-exports/translations.csv';
-var includeWordCount = true;
+let program = require('commander');
+let fs = require('fs');
+let flatten = require('flat');
+let csvWriter = require('csv-write-stream');
+let wordCount = require('wordcount');
+let arrayContains = require('array-contains');
+
+const ENCODING = 'UTF-8';
+const TRANSLATION_BASE_DIR = 'app/locales/';
+const TRANSLATION_FILE_NAME = 'translations.js';
+const DEFAULT_OUTPUT_DIR = 'i18n-exports';
+const DEFAULT_OUTPUT_FILE = 'translations.csv';
+const DEFAULT_TRANSLATION_KEY_COLUMN_NAME = 'SYSTEM_KEY';
+const DEFAULT_WORD_COUNT_SUFFIX = '_WORD_COUNT';
+
+let showWordCount = true;
+let outputDir = DEFAULT_OUTPUT_DIR;
+let outputFile = DEFAULT_OUTPUT_FILE;
+let localeColumnNames = {};
 
 program
-  .version('1.0.0')
-  .option('--outputFile [outputFile]', 'The output csv file. Defaults to i18n-exports/translations.cvs')
-  .option('--outputDir [outputDir]', 'The output directory. Defaults to i18n-exports directory')
-  .option('--includeWordCount [includeWordCount]', 'Include word counts. Default to true')
-  .parse(process.argv);
+	.version('1.0.0')
+	.option('--outputFile [outputFile]', 'The output csv file. Defaults to translations.cvs')
+	.option('--outputDir [outputDir]', 'The output directory. Defaults to i18n-exports')
+	.option('--showWordCount [showWordCount]', 'Show word counts. Default to true')
+	.option('--translationKeyColumnName [translationKeyColumnName]',
+		'The column name for the translation key. Defaults to SYSTEM_KEY')
+	.option('--localeColumnNames [localeColumnNames]', 'The column names for each locales. Use the locale name as the key. ' +
+		'Defaults to {\\\"en\\\:\\\"EN\\\",\\\"fr\\\": \\\"FR\\\"}')
+	.option('--wordCountColumnNameSuffix [wordCountColumnNameSuffix]', 'The suffix appended for word count columns. Default to WORD_COUNT')
+	.parse(process.argv);
 
-  	if (program.includeWordCount === "false") {
-  		includeWordCount = false;
-  	}
+if (program.showWordCount === 'false') {
+	showWordCount = false;
+}
 
-	if (!fs.existsSync(OUTPUT_DIR)) {
-		fs.mkdirSync(OUTPUT_DIR);
+if (program.outputFile) {
+	outputFile = program.outputFile;
+}
+
+if (program.outputDir) {
+	outputDir = program.outputDir;
+}
+
+if (program.localeColumnNames) {
+	localeColumnNames = JSON.parse(program.localeColumnNames);
+}
+
+generateFile();
+
+function generateFile() {
+
+	//get output file path.
+	let outputFilePath = `${outputDir}/${outputFile}`;
+
+	if (!fs.existsSync(outputDir)) {
+		fs.mkdirSync(outputDir);
 	}
 
-	fs.unlink(OUTPUT_FILE, (err) => {
+	if (fs.existsSync(outputFilePath)) {
+		fs.unlinkSync(outputFilePath);
+	}
 
-		console.log('Generating translation export...')
+	let writer = csvWriter();
 
-		let writer = csvWriter();
+	writer.pipe(fs.createWriteStream(outputFilePath, {
+		defaultEncoding: 'binary',
+	}));
 
-		writer.pipe(fs.createWriteStream(OUTPUT_FILE, {
-			defaultEncoding: 'binary',
-		}));
+	let translationMap = getTranslationMap();
+	let translationKeys = getTranslationKeys(translationMap);
 
-		let translationMap = getTranslationMap();
-		let translationKeys = getTranslationKeys(translationMap);
+	writeRow(writer, null, translationMap);
 
-		writeRow(writer, null, translationMap);
-
-		translationKeys.forEach(function (translationKey) {
-			writeRow(writer, translationKey, translationMap);
-		});
-
-		writeRow(writer, null, translationMap);
-
-		if (includeWordCount) {
-			writeFooterRow(writer, translationMap);
-		}
-
-		writer.end();
+	translationKeys.forEach(function (translationKey) {
+		writeRow(writer, translationKey, translationMap);
 	});
 
+	writeRow(writer, null, translationMap);
 
-	function getLocales() {
-		return fs.readdirSync(TRANSLATION_BASE_DIR).filter(function (file) {
-			return fs.statSync(TRANSLATION_BASE_DIR + file).isDirectory();
-		});
+	if (showWordCount) {
+		writeCountSummaryRow(writer, translationMap);
 	}
 
-	function getTranslations(data) {
-		let translationsStartIndex = data.indexOf('{');
-		let translationsEndIndex = data.lastIndexOf('}');
-		if (translationsStartIndex < 0) {
-			throw new Error('Unable to parse the translations.');
+	writer.end();
+}
+
+function getWordCountColumnNameSuffix() {
+	if (program.wordCountColumnNameSuffix) {
+		return "_" + program.wordCountColumnNameSuffix.toUpperCase();
+	}
+	return DEFAULT_WORD_COUNT_SUFFIX;
+}
+
+/**
+ * Returns the column name for a locale.
+ * @param locale The locale
+ * @returns {string}
+ */
+function getLocaleColumnName(locale) {
+	let columnName = localeColumnNames[locale];
+	if (!columnName) {
+		columnName = locale;
+	}
+	return columnName.toUpperCase();
+}
+
+/**
+ * Returns the column name for the translation key.
+ * defaults to TRANSLATION_KEY
+ * @returns {String}
+ */
+function getTranslationKeyColumnName() {
+	if (program.translationKeyColumnName) {
+		return program.translationKeyColumnName.toUpperCase();
+	}
+	return DEFAULT_TRANSLATION_KEY_COLUMN_NAME;
+}
+
+/**
+ * Returns the list of locales based on the ember-i18n file directory.
+ * @returns {Array}
+ */
+function getLocales() {
+	// loop throught the list of subdirectories in app/locales
+	return fs.readdirSync(TRANSLATION_BASE_DIR).filter(function (file) {
+		return fs.statSync(TRANSLATION_BASE_DIR + file).isDirectory();
+	});
+}
+
+/**
+ * Returns the translations from the translations data
+ * @param translationsData The translations data.
+ */
+function getTranslations(translationsData) {
+	// find where the translations JSON starts.
+	let translationsStartIndex = translationsData.indexOf('{');
+	// find where the translations JSON ends.
+	let translationsEndIndex = translationsData.lastIndexOf('}');
+	if (translationsStartIndex < 0 || translationsEndIndex < 0) {
+		throw new Error('Unable to parse the translations.');
+	}
+	// Parse JSON string
+	let translations = JSON.parse(translationsData.substring(translationsStartIndex, translationsEndIndex + 1));
+	if (!translations) {
+		throw new Error('Unable to parse the translations.');
+	}
+	return translations;
+}
+/**
+ * Returns the flat version of the translations from the translation data.
+ * @param translationData The translations
+ * @returns {object}
+ */
+function getFlattenTranslations(translationData) {
+	// flatten JSON object.
+	let flattenTranslations = flatten(getTranslations(translationData));
+	if (!flattenTranslations) {
+		throw new Error('Unable to flatten the translations.');
+	}
+	return flattenTranslations;
+}
+
+/**
+ * Reads all transitions files returns a nested JSON object where the
+ * first level key is the locale and second level key is the translation key ex:
+ * {
+ * 	"en": {
+ * 		"component1.label.field1": "value"
+ * 	},
+ * 	"fr": {
+ * 		"component1.label.field1": "value [fr]"
+ * 	}
+ * }
+ * @returns {Object}
+ */
+function getTranslationMap() {
+	let locales = getLocales();
+	let translationsMap = {};
+	// loop through each locale and retrieve the translations file.
+	locales.forEach(function (locale) {
+		let translationFilePath = `${TRANSLATION_BASE_DIR}${locale}/${TRANSLATION_FILE_NAME}`;
+		// if there no transition file but a locale leave  it blank.
+		if (fs.existsSync(translationFilePath)) {
+			let data = fs.readFileSync(translationFilePath, ENCODING);
+			translationsMap[locale] = getFlattenTranslations(data);
+		} else {
+			translationsMap[locale] = {};
 		}
-		let translations = JSON.parse(data.substring(translationsStartIndex, translationsEndIndex + 1));
-		if (!translations) {
-			throw new Error('Unable to parse the translations.');
-		}
-		return translations;
-	}
+	});
+	return translationsMap;
+}
 
-	function getFlattenTranslations(data) {
-		// flatten JSON object.
-		let flattenTranslations = flatten(getTranslations(data));
-		if (!flattenTranslations) {
-			throw new Error('Unable to flatten the translations.');
-		}
-		return flattenTranslations;
-	}
-
-	function getTranslationMap() {
-		let locales = getLocales();
-		let translationsMap = {};
-		locales.forEach(function (locale) {
-			let translationFilePath = `${TRANSLATION_BASE_DIR}${locale}/${TRANSLATION_FILE_NAME}`;
-			if (fs.existsSync(translationFilePath)) {
-				let data = fs.readFileSync(translationFilePath, ENCODING);
-				translationsMap[locale] = getFlattenTranslations(data);
-			} else {
-				translationsMap[locale] = {};
-			}
-		});
-		return translationsMap;
-	}
-
-	function getTranslationKeys(translationMap) {
-		const translationKeys = [];
-		for (let localeKey in translationMap) {
-			if (translationMap.hasOwnProperty(localeKey)) {
-				for (let translationKey in translationMap[localeKey]) {
-					if (translationMap[localeKey].hasOwnProperty(translationKey)
-						&& !arrayContains(translationKeys, translationKey)) {
-						translationKeys.push(translationKey);
-					}
+/**
+ * Returns a array of translations keys for a given translations map
+ * @param translationMap The translations map
+ * @returns {Array}
+ */
+function getTranslationKeys(translationMap) {
+	const translationKeys = [];
+	// loop through all locale and translation keys.
+	for (let locale in translationMap) {
+		if (translationMap.hasOwnProperty(locale)) {
+			for (let translationKey in translationMap[locale]) {
+				if (translationMap[locale].hasOwnProperty(translationKey)
+					&& !arrayContains(translationKeys, translationKey)) {
+					translationKeys.push(translationKey);
 				}
 			}
 		}
-		return translationKeys;
 	}
+	return translationKeys;
+}
 
-	function writeRow(writer, translationKey, translationMap) {
-		let row = {
-			SYSTEM_KEY: translationKey
-		};
-		for (let localeKey in translationMap) {
-			if (translationMap.hasOwnProperty(localeKey)) {
-				const translation = translationMap[localeKey][translationKey];
-				row[localeKey.toUpperCase()] = translation;
-				if (includeWordCount) {
-					if (translation) {
-                		row[`${localeKey.toUpperCase()}_WORD_COUNT`] = wordCount('' + translation);//
-                	} else {
-                		row[`${localeKey.toUpperCase()}_WORD_COUNT`] = null;
-                	}
+/**
+ * Write a row in the csv file.
+ * @param writer The csv writer.
+ * @param translationKey The translation key.
+ * @param translationMap The translation map.
+ */
+function writeRow(writer, translationKey, translationMap) {
+	let row = {};
+	row[getTranslationKeyColumnName()] = translationKey;
+	for (let locale in translationMap) {
+		if (translationMap.hasOwnProperty(locale)) {
+			const translation = translationMap[locale][translationKey];
+			row[getLocaleColumnName(locale)] = translation;
+			if (showWordCount) {
+				if (translation) {
+					row[`${getLocaleColumnName(locale)}${getWordCountColumnNameSuffix()}`] = wordCount('' + translation);//
+				} else {
+					row[`${getLocaleColumnName(locale)}${getWordCountColumnNameSuffix()}`] = null;
 				}
 			}
 		}
-		writer.write(row);
 	}
+	writer.write(row);
+}
 
-	function writeFooterRow(writer, translationMap) {
-		let row = {
-			SYSTEM_KEY: ''
-		};
-		for (let localeKey in translationMap) {
-			let localeWordCount = 0;
-			if (translationMap.hasOwnProperty(localeKey)) {
-				for (let translationKey in translationMap[localeKey]) {
-					if (translationMap[localeKey].hasOwnProperty(translationKey)) {
-						localeWordCount = localeWordCount + wordCount(translationMap[localeKey][translationKey]);
-					}
+/**
+ * Write the count summary for each locale.
+ * @param writer The csv writer
+ * @param translationMap The translation map.
+ */
+function writeCountSummaryRow(writer, translationMap) {
+	let row = {};
+	row[getTranslationKeyColumnName()] = '';
+	for (let localeKey in translationMap) {
+		let localeWordCount = 0;
+		if (translationMap.hasOwnProperty(localeKey)) {
+			for (let translationKey in translationMap[localeKey]) {
+				if (translationMap[localeKey].hasOwnProperty(translationKey)) {
+					localeWordCount = localeWordCount + wordCount(translationMap[localeKey][translationKey]);
 				}
-				row[localeKey.toUpperCase()] = '';
-				row[`${localeKey.toUpperCase()}_WORD_COUNT`] = `Total words: ${localeWordCount} `;//
 			}
+			row[getLocaleColumnName(localeKey)] = '';
+			row[`${getLocaleColumnName(localeKey)}${getWordCountColumnNameSuffix()}`] = `Total words: ${localeWordCount} `;//
 		}
-		writer.write(row);
 	}
+	writer.write(row);
+}
